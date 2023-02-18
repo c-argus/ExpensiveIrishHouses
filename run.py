8315ea52a9aa5e262a6f0bac319c75c5f68acbc7
c-argus <camila.argus88@gmail.com>
2023-02-18 11:06:05 +0000

    Create input menu to start the program

--- run.py
+++ run.py
@@ -1,1 +1,9 @@
-
+"""
+Get user to input an option on the menu.
+Using ()upper to  converts characters to uppercase, 
+so the user can input uppercase or not 
+and it won't get an error message back
+"""
+print ("Choose your option on the menu below:\n")
+while True:
+    op = input ("L - List transactions \nA - Add transaction \nD - Delete transaction \nE - Edit transaction \nC - Check balance \nE - Exit \n\nType here").upper()
