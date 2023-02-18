f528f9c957b6b8d3dd7d4353e8b2f4c1c892b56c
c-argus <camila.argus88@gmail.com>
2023-02-18 13:12:02 +0000

    Add menu functions to be edited in the future

--- run.py
+++ run.py
@@ -1,9 +1,32 @@
 """
+Menu functions
+"""
+
+def listTransactions():
+    pass
+
+def addTransaction():
+    pass
+
+def deleteTransaction():
+    pass
+
+def editTransaction():
+    pass
+
+def checkBalance():
+    pass
+
+
+"""
 Get user to input an option on the menu.
-Using ()upper to  converts characters to uppercase, 
-so the user can input uppercase or not 
+Using ()upper to  converts characters to uppercase,
+so the user can input uppercase or not
 and it won't get an error message back
 """
+
 print ("Choose your option on the menu below:\n")
+
 while True:
     op = input ("L - List transactions \nA - Add transaction \nD - Delete transaction \nE - Edit transaction \nC - Check balance \nE - Exit \n\nType here").upper()
+
